474c21a9af1eb85dcea4c2d12dd8d6aa7a5ed78b
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-11-03 13:31:41 +0000

    update

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3000;
  
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
+app.use('/', require('./routes'));
+
  
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
